a69836a1a48896bf19bcb0641e39ff8f36b48548
Caio Viana <caiofeviana@gmail.com>
2021-07-28 15:35:42 -0300

    fufu... it works

--- res/preload_charpage.js
+++ res/preload_charpage.js
@@ -3,6 +3,10 @@
         if (variable == undefined || variable == null) return true;
         else return false;
     }
+    // Thanks google
+    function _getHexColor(int){
+        return "#"+((int)>>>0).toString(16).slice(-6);
+    }
     
     function onLoading () {
         // Get necessary data first---------------
@@ -16,7 +20,7 @@
         // POLISHING PAGE
         var body = document.body;
         body.innerHTML = "";
-        body.style.backgroundColor = "#000000";//"#FEF1C5";
+        body.style.backgroundColor = "#FEF1C5";
         
         if (!_isnull(flash)) {
             // VALID FLASH PAAAGE! Lets go!
@@ -60,19 +64,17 @@
         const extractIntFromCP = (attributeName) => {
             // Ex: ...smthsmth&intEyesCC=443322&smthsmth...
             // We want the numbers for the intEyes in the ex.
-            var a = sourceFlash.split(attributeName + "=")[1].split('&')[0];
-            // TESTING
-            console.log(attributeName + ": " + a + "\n");
-            return a;
+            return sourceFlash.split(attributeName + "=")[1].split('&')[0];
         }
-        
-        const ccHair = extractIntFromCP("intColorHair")
-        const ccEyes = extractIntFromCP("intColorEye")
-        const ccSkin = extractIntFromCP("intColorSkin")
-        const ccBase = extractIntFromCP("intColorBase")
-        const ccTrim = extractIntFromCP("intColorTrim")
-        const ccAces = extractIntFromCP("intColorAccessory")
-        console.log("\n\n")
+        return {
+            ccHair : extractIntFromCP("intColorHair"),
+            ccSkin : extractIntFromCP("intColorSkin"),
+            ccEyes : extractIntFromCP("intColorEye"),
+
+            ccBase : extractIntFromCP("intColorBase"),
+            ccTrim : extractIntFromCP("intColorTrim"),
+            ccAces : extractIntFromCP("intColorAccessory")
+        }
         
         // Healerbounty - Main set with Naval DK for example
         // intColorHair=2236962&intColorSkin=15448438&intColorEye=6684672&intColorTrim=15726591&intColorBase=2368548&intColorAccessory=2368548
@@ -83,6 +85,7 @@
         
         //TODO - VW is only for bigger width ratio. change for VH later if needed
         
+        // Same size as flash, over it, to serve as guide
         var flashFrame = document.createElement("div");
         flashFrame.style.width = w + "px"
         flashFrame.style.height = h + "px"
@@ -93,6 +96,7 @@
         flashFrame.style.zIndex = 50;
         //flashFrame.style.backgroundColor = "#FF000099"
         
+        // Container for the CC colors themselfs, Cover the "profile picture"
         var CCContainer = document.createElement("div");
         CCContainer.style.position = "absolute"
         CCContainer.style.width = 0.3*w + "px";
@@ -100,12 +104,43 @@
         CCContainer.style.zIndex = 51;
         CCContainer.style.left = 0.005*w + "px";
         CCContainer.style.bottom = 0.07*h + "px";
-        CCContainer.style.backgroundColor = "blue";
+        //CCContainer.style.backgroundColor = "#FFFF00";
+
+        // The cells themselfs, in the order shown in aqw.
+        // Top row is Hair, Skin, Eye
+        // Bottom row is Base, Trim, Accessory
+        // Pass posX and posY as would be in a Matrix index.
+        // Color value doesnt have a #.
+        const makeColorCell = (posX,posY,colorValue) => {
+            var cell = document.createElement("div");
+            cell.style.width  =   0.1*w + "px";
+            cell.style.height = 0.075*h + "px";
+            cell.style.top  = 0.075*h*(posX-1) + "px";
+            cell.style.left =   0.1*w*(posY-1) + "px";
+            cell.style.position = "absolute"
+            cell.style.backgroundColor = _getHexColor(colorValue);
+            return cell;
+        }
+
+        // Make the cells
+        const cpValues = getCC();
+        var cellHair = makeColorCell(1,1,cpValues.ccHair)
+        var cellSkin = makeColorCell(1,2,cpValues.ccSkin)
+        var cellEyes = makeColorCell(1,3,cpValues.ccEyes)
+        var cellBase = makeColorCell(2,1,cpValues.ccBase)
+        var cellTrim = makeColorCell(2,2,cpValues.ccTrim)
+        var cellAces = makeColorCell(2,3,cpValues.ccAces)
+
+        // Build them
+        CCContainer.appendChild(cellHair);
+        CCContainer.appendChild(cellSkin);
+        CCContainer.appendChild(cellEyes);
+        CCContainer.appendChild(cellBase);
+        CCContainer.appendChild(cellTrim);
+        CCContainer.appendChild(cellAces);
         flashFrame.appendChild(CCContainer);
-        
-        
-        
         document.body.appendChild(flashFrame);
+
     }
     
     window.onload= onLoading;
